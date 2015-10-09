63ecdfd5a375134c23b486775a49069e9b4f6de2
Matt Brennan <mattyb1000@gmail.com>
2015-10-09 08:58:00 +0100

    shitty curves

--- index.js
+++ index.js
@@ -20,8 +20,29 @@ var transpose = rows => rows.reduce(zipWith((col, x) => col.concat([x])), rows[0
 
 var normaliseAll = (max, data) => transpose(transpose(data).map((xs, i) => xs.map(normalise(max[i], ...bounds(xs)))));
 
-console.log(normaliseAll([300, 200], [
+var group = n => xs => (
+	  xs.length < n? []
+	: [xs.slice(0, n), ...group(n)(xs.slice(1))]
+);
+
+var c = document.createElement('canvas');
+var dims = [800, 600];
+[c.width, c.height] = dims;
+[c.style.width, c.style.height] = dims.map(x => x/2 + 'px');
+document.body.appendChild(c);
+var ctx = c.getContext('2d');
+
+ctx.beginPath();
+group(2)(normaliseAll(dims, [
 	[1, 1],
-	[2, 3],
+	[2, 4],
 	[3, 5],
-]))
+])).forEach(([[x1, y1], [x2, y2]]) => {
+	ctx.moveTo(x1, dims[1] - y1);
+	ctx.bezierCurveTo(
+		x1 * 1.5, dims[1] - y1,
+		x2 * 0.5, dims[1] - y2,
+		x2, dims[1] - y2
+	);
+});
+ctx.stroke();
