b1cffb26948e6a023ffaf4cfad4c889eae13a5c3
Matt Brennan <mattyb1000@gmail.com>
2015-10-10 22:37:36 +0100

    ooh

--- test.js
+++ test.js
@@ -1,36 +1,40 @@
 var graph = require('./');
 
-function create() {
+document.body.style.background = '#44505B';
+
+function create(fg) {
 	var c = document.createElement('canvas');
 	var dims = [window.innerWidth, 100];
 	[c.width, c.height] = dims.map(x => x * window.devicePixelRatio);
 	[c.style.width, c.style.height] = dims.map(x => x + 'px');
+	c.style.position = 'absolute';
+	c.style.top = '10px';
+	c.style.left = '0px';
 	document.body.appendChild(c);
 
 	var data = Array.from(Array(20)).map((_, i) => [
-		Date.now() - 20000 + i * 1000, Math.random()
+		Date.now() - 20000 + i * 1000, Math.random()*0.9+0.05
 	]);
 
 	setInterval(function () {
 		if(data[data.length - 1][0] < Date.now() - 1000) {
-			data.push([Date.now(), Math.random()]);
+			data.push([Date.now(), Math.random()*0.9+0.05]);
 		}
 
 		if(data[0][0] < Date.now() - 22000) data.shift();
 	}, 1000);
 
-	document.body.style.background = '#44505B';
 
 	(function draw() {
 		graph(c, data, {bounds: [[Date.now() - 20000, Date.now() - 2000], [0, 1]], pathStyle(ctx) {
 			ctx.lineWidth = 2 * devicePixelRatio;
-			ctx.strokeStyle = '#fff1e0';
+			ctx.strokeStyle = fg;
 		}, pre(ctx) {
 			ctx.translate(0, ctx.canvas.height);
 			ctx.scale(1, -1);
 			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 		}, pointStyle(ctx) {
-			ctx.fillStyle = '#fff1e0';
+			ctx.fillStyle = fg;
 		}, drawPoint(ctx, [x, y]) {
 			ctx.beginPath();
 			ctx.globalCompositeOperation = 'destination-out';
@@ -39,7 +43,7 @@ function create() {
 			ctx.globalCompositeOperation = 'source-over';
 
 			ctx.beginPath();
-			ctx.fillStyle = '#fff1e0';
+			ctx.fillStyle = fg;
 			ctx.ellipse(x, y, 2 * devicePixelRatio, 2 * devicePixelRatio, 0, 0, 2 * Math.PI);
 			ctx.fill();
 		}, postPoint() {}, prePoint() {}});
@@ -47,8 +51,4 @@ function create() {
 	}());
 }
 
-create();
-create();
-create();
-create();
-create();
+require('pleasejs').make_color({colors_returned: 4}).map(create);
